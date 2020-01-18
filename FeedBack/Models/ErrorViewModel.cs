dbb4c021021105d813e150c6e5b82f018e2b2b18
HARKULAS <HARKULAS@HARKULAS>
2020-01-18 22:31:55 +0545

    Add project files.

---
+++ FeedBack/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace FeedBack.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
