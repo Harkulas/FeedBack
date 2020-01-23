6da8dcce55629852b305e3806b3c4fbdc8662bbe
HARKULAS <HARKULAS@HARKULAS>
2020-01-24 01:34:00 +0545

    New Changes as Login

--- FeedBack/Controllers/AccountController.cs
+++ FeedBack/Controllers/AccountController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using FeedBack.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace FeedBack.Controllers
@@ -11,6 +12,33 @@ namespace FeedBack.Controllers
         public IActionResult Login()
         {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(LoginVModel loginVmodel)
+        {
+            try
+            {
+                var name = "admin@gmail.com";
+                var pass = "admin";
+                if (ModelState.IsValid)
+                {
+                    if (name == loginVmodel.Email && pass == loginVmodel.Password)
+                        return RedirectToAction("Index", "Home");
+                    else
+                        return Ok("Invalid Email or Password");
+                }
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError(string.Empty, "Invalid Email or Password");
+            }
+            return View(loginVmodel);
+        }
+
+        public IActionResult Logout()
+        {
+            return RedirectToAction("Login", "Account");
         }
     }
 }
